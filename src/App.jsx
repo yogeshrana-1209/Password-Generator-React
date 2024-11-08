b565fc4ba133f77205036e2492ad9c7242f092b2
Yogesh Rana <yr70416@gmail.com>
2024-11-08 13:38:41 +0530

    Edited in file App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ function App() {
   }, [length, numberAllowed, charAllowed, setPassword])
 
   const copyPasswordToClipboard = useCallback(() => {
-      passwordRef.current?.select();
-      passwordRef.current?.setSelectionRange(0, 999);
-      window.navigator.clipboard.writeText(Password)
-  },[Password]);
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(Password)
+  }, [Password]);
 
   useEffect(() => {
     passwordGenerator()
@@ -38,9 +38,18 @@ function App() {
 
   return (
     <>
+
+      <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="https://i.pinimg.com/originals/f7/61/3c/f7613cabf85632929fb9612a47f3dfe3.gif" alt="" />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">React Project</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Developed By : Yogesh Rana <br/>Deployed on 08/11/2024, v-0.1.2 </p>
+        </div>
+      </a>
+
       {/* <h1 className='text-4xl text-center text-white'>Password Generator</h1> */}
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg border-solid border-2 border-sky-500 px-4 py-3 my-8 text-orange-500 bg-gray-700'>
-        <h1 className='text-white text-center px-5 py-5 my-3'>Password Generator</h1>
+      <div className='w-full max-w-sm mx-auto shadow-md rounded-lg border-solid border-2 border-sky-500 px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white pb-5 pt-5">Password Generator</h5>
         {/* <h1>Developed by : Yogesh Rana</h1> */}
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type="text"
@@ -50,9 +59,9 @@ function App() {
             readOnly
             ref={passwordRef}
           />
-          <button 
-          onClick={copyPasswordToClipboard}
-          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          <button
+            onClick={copyPasswordToClipboard}
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
           >copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
@@ -80,7 +89,7 @@ function App() {
                 setCharAllowed((prev) => !prev);
               }}
             />
-            <label htmlFor="characterInput">Characters</label>
+            <label htmlFor="characterInput">Char</label>
           </div>
         </div>
       </div>
